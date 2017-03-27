5b0afd820251c5f20e7dd937c65ce795478b2bea
Ricardo <rr.ricci@gmail.com>
2017-03-27 14:46:30 +0200

    TP1

---
+++ TP1/ContactManager2/src/m1/piu/ContactManagerFX.java
@@ -0,0 +1,38 @@
+package m1.piu;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author adesv
+ */
+public class ContactManagerFX extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLMainFrame.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
